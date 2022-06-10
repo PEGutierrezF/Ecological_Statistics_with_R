6f73084a6a19fd6ef942af2dfd4512625a60a8d1
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-06-10 15:35:38 -0400

    data

---
+++ libraries.R
@@ -0,0 +1,16 @@
+
+
+
+
+# ---------------------------------------------
+# Libraries
+# 10 Jun 2022
+# Pablo E. Gutiérrez-Fonseca
+# pabloe.gutierrezfonseca@gmail.com
+# ---------------------------------------------
+#  
+
+
+
+install.packages("bookdown")
+library(bookdown)
